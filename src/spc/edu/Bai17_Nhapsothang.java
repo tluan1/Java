fe0da0f987466bf03d890c1c9b91c108e2ee9170
luanthanh253@gmail.com <Administrator@LAB1-MAY15>
2024-09-17 16:50:03 +0700

    e

---
+++ src/spc/edu/Bai17_Nhapsothang.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package spc.edu;
+import java.util.Scanner;
+/**
+ *
+ * @author Administrator
+ */
+public class Bai17_Nhapsothang {
+    public static void main(String[] args) {
+        System.out.println("Tinh so thang: ");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Nhap so thang co : ");
+        int sothang = sc.nextInt();
+        System.out.println("Thang " + sothang + " co 31 ngay ");
+        System.out.println("Thang " + sothang + " co 30 ngay ");
+        System.out.println("Thang " + sothang + " co 28 hoac 29 ngay ");
+    }
+}
