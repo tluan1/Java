60eed2ba64ed7e287a71da00dfd16edd86d7e121
luanthanh253@gmail.com <Administrator@LAB1-MAY15>
2024-09-17 09:00:29 +0700

    e

---
+++ src/spc/edu/Bai11_Giaithua.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package spc.edu;
+import java.util.Scanner;
+/**
+ *
+ * @author Administrator
+ */
+public class Bai11_Giaithua {
+    public static void main(String[] args) {
+        System.out.println("Giai thua");
+        Scanner sc = new Scanner(System.in);
+        int i = 1, sonhap, giaithua = 1;
+        System.out.println("Nhap so can tinh giai thua:");
+        sonhap = sc.nextInt();
+        giaithua = giaithua * i;
+        System.out.println("giai thua cua" + sonhap + "la: " + giaithua);
+    }
+}
