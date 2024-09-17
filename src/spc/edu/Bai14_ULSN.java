9420fa50308943c62318fefe13ea4c4632ada753
luanthanh253@gmail.com <Administrator@LAB1-MAY15>
2024-09-17 15:13:00 +0700

    e

---
+++ src/spc/edu/Bai14_ULSN.java
@@ -0,0 +1,45 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package spc.edu;
+import java.util.Scanner;
+/**
+ *
+ * @author Administrator
+ */
+public class Bai14_ULSN {
+    public static int nhap()
+    {
+        Scanner sc = new Scanner(System.in);
+        boolean check = false;
+        int n = 0;
+        while(!check){
+            System.out.print("");
+            try{
+                n = sc.nextInt();
+                check = true;
+            }
+            catch(Exception e){
+                System.out.println("Ban phai nhap so! hay nhap lai...");
+                sc.nextLine();
+            }
+        }
+        return(n);
+    }
+    public static int UCLN(int a, int b){
+        while(a!=b){
+            if(a > b) a = a - b;
+            else b = b - a;
+        }
+        return(a);
+    }
+    public static void main(String[] args) {
+        System.out.println("Nhap a: ");
+        int a = nhap();
+        System.out.println("Nhap b: ");
+        int b = nhap();
+        System.out.println("Uoc chung lon nhat cua " + a + " va " + b + " la: " + UCLN(a, b));
+        System.out.println("Boi chung nho nhat cua " + a + " va " + b + " la: " +((a * b)/UCLN(a, b)));
+    }
+}
