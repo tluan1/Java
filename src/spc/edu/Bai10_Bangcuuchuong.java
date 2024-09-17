48460496f2e533fa12414e11f4ef5be200d97521
luanthanh253@gmail.com <Administrator@LAB1-MAY15>
2024-09-17 08:27:58 +0700

    e

---
+++ src/spc/edu/Bai10_Bangcuuchuong.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package spc.edu;
+/**
+ *
+ * @author Administrator
+ */
+public class Bai10_Bangcuuchuong {
+    public static void main(String[] args) {
+        System.out.println("Bảng cửu chương");
+        int i,j;
+        for( i = 1; i <= 10; i++){
+            for( j = 1; j <= 10; j++){
+                System.out.println(i + " x " + j + " = " + i*j);
+            }
+        }
+    }
+}
