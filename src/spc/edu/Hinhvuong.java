3be113a2e391b42871fcf3ece6479391a9359575
luanthanh253@gmail.com <Administrator@LAB1-MAY15>
2024-10-01 16:31:26 +0700

    i

---
+++ src/spc/edu/Hinhvuong.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package spc.edu;
+
+/**
+ *
+ * @author Administrator
+ */
+public class Hinhvuong extends Hinhchunhat {
+    public Hinhvuong(){
+        super();
+    }
+    public Hinhvuong(int dorong){
+        super(dorong, dorong);
+    }
+}
